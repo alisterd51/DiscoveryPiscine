bbd9e566f3242951c783c26d2b7ece4de3dc3148
anclarma <anclarma@student.42.fr>
2021-10-06 18:52:01 +0200

    ajout fin ex 3.1

--- cell03/ex01/balloon.js
+++ cell03/ex01/balloon.js
@@ -1,29 +1,57 @@
+function	color_next(color)
+{
+	if (color == "rgb(255, 0, 0)")
+		color = "rgb(0, 255, 0)";
+	else if (color == "rgb(0, 255, 0)")
+		color = "rgb(0, 0, 255)";
+	else if (color == "rgb(0, 0, 255)")
+		color = "rgb(255, 0, 0)";
+	return (color);
+}
+
+function	color_previous(color)
+{
+	if (color == "rgb(255, 0, 0)")
+		color = "rgb(0, 0, 255)";
+	else if (color == "rgb(0, 255, 0)")
+		color = "rgb(255, 0, 0)";
+	else if (color == "rgb(0, 0, 255)")
+		color = "rgb(0, 255, 0)";
+	return (color);
+}
+
 function	balloon_inflation()
 {
 	var	width;
 	var	height;
 	let	color;
-	let	new_color;
 
 	width = document.getElementById("balloon").offsetWidth + 10;
 	height = document.getElementById("balloon").offsetHeight + 10;
 	color = window.getComputedStyle(document.getElementById("balloon")).backgroundColor;
-	document.getElementById('balloon').style.width = width + "px";
-	document.getElementById('balloon').style.height = height + "px";
-	if (color == 'rgb(255, 0, 0)')
-		new_color = 'rgb(0, 255, 0)';
-	else if (color == 'rgb(0, 255, 0)')
-		new_color = 'rgb(0, 0, 255)';
-	else if (color == 'rgb(0, 0, 255)')
-		new_color = 'rgb(255, 0, 0)';
-	document.getElementById('balloon').style.backgroundColor = new_color;
+	document.getElementById("balloon").style.width = width + "px";
+	document.getElementById("balloon").style.height = height + "px";
+	document.getElementById("balloon").style.backgroundColor = color_next(color);
 	if (width > 420 || height > 420)
 	{
-		document.getElementById('balloon').style.width = "200px";
-		document.getElementById('balloon').style.height = "200px";
+		document.getElementById("balloon").style.width = "200px";
+		document.getElementById("balloon").style.height = "200px";
 	}
 }
 
 function	balloon_deflation()
 {
+	var	width;
+	var	height;
+	let	color;
+
+	width = document.getElementById("balloon").offsetWidth - 5;
+	height = document.getElementById("balloon").offsetHeight - 5;
+	color = window.getComputedStyle(document.getElementById("balloon")).backgroundColor;
+	if (width >= 200 || height >= 200)
+	{
+		document.getElementById("balloon").style.width = width + "px";
+		document.getElementById("balloon").style.height = height + "px";
+		document.getElementById("balloon").style.backgroundColor = color_previous(color);
+	}
 }
